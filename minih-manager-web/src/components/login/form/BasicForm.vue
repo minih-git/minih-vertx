b86c5e77bd6568c4d52ca0cda131de020966ce7d
hubin <hub@hungraim.com>
2023-07-25 17:32:17 +0800

    优化项目结构，完善框架

--- minih-manager-web/src/components/login/form/BasicForm.vue
+++ minih-manager-web/src/components/login/form/BasicForm.vue
@@ -1,44 +1,44 @@
 <template>
-    <div class="wrapper input-wrapper">
-        <div class="input-icon">
-            <el-icon>
-                <User/>
-            </el-icon>
-        </div>
-        <div class="input-con">
-            <input v-model="formData.username" class="input" placeholder="账号" type="text">
-        </div>
+  <div class="wrapper input-wrapper">
+    <div class="input-icon">
+      <el-icon>
+        <User/>
+      </el-icon>
     </div>
-    <div class="wrapper input-wrapper">
-        <div class="input-icon">
-            <el-icon>
-                <Lock/>
-            </el-icon>
-        </div>
-        <div class="input-con">
-            <input v-model="formData.password" :type="showPwd?'password':'text'" class="input" placeholder="密码"
-                   @keydown.enter="onSubmit">
-        </div>
-        <div class="input-icon icon-click" style="cursor: pointer" @click="showPwd=!showPwd">
-            <el-icon>
-                <Hide v-if="showPwd"/>
-                <View v-else/>
-            </el-icon>
-        </div>
+    <div class="input-con">
+      <input ref="accountRef" v-model="formData.username" class="input" placeholder="账号" type="text">
     </div>
-    <div class="wrapper save-p-wrapper">
-        <el-checkbox v-model="savePassword" label="保存密码" size="small"/>
+  </div>
+  <div class="wrapper input-wrapper">
+    <div class="input-icon">
+      <el-icon>
+        <Lock/>
+      </el-icon>
     </div>
-    <div class="wrapper submit-wrapper">
-        <div class="submit-btn" @click="onSubmit">
-            <div v-show="isLoading" id="loading">
-                <el-icon :class="{'is-loading':isLoading}">
-                    <Loading/>
-                </el-icon>
-            </div>
-            登录
-        </div>
+    <div class="input-con">
+      <input ref="pwdRef" v-model="formData.password" :type="showPwd?'password':'text'" class="input" placeholder="密码"
+             @keydown.enter="onSubmit">
     </div>
+    <div class="input-icon icon-click" style="cursor: pointer" @click="showPwd=!showPwd">
+      <el-icon>
+        <Hide v-if="showPwd"/>
+        <View v-else/>
+      </el-icon>
+    </div>
+  </div>
+  <div class="wrapper save-p-wrapper">
+    <el-checkbox v-model="savePassword" label="保存密码" size="small"/>
+  </div>
+  <div class="wrapper submit-wrapper">
+    <div class="submit-btn" @click="onSubmit">
+      <div v-show="isLoading" id="loading">
+        <el-icon :class="{'is-loading':isLoading}">
+          <Loading/>
+        </el-icon>
+      </div>
+      登录
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -46,36 +46,57 @@ import {reactive, ref} from "vue";
 import {Hide, Loading, Lock, User, View} from "@element-plus/icons-vue";
 import {FormInfo, info, login} from "../../../api";
 import {useRouter} from 'vue-router'
+import {ElMessage} from "element-plus";
 
 
 const showPwd = ref<Boolean>(true)
 const config = JSON.parse(localStorage.getItem("config") || "{}")
 const savePassword = ref<Boolean>(config.savePassword === true)
 let isLoading = ref(false)
+let accountRef = ref()
+let pwdRef = ref()
 const router = useRouter()
 
 const formData = reactive<Partial<FormInfo>>({
-    username: config?.localUser,
-    password: config?.localPass,
+  username: config?.localUser,
+  password: config?.localPass,
 })
 const onSubmit = async () => {
-    isLoading.value = true
-    try {
-        await login(formData)
-        await info()
-        config.savePassword = savePassword.value
-        config.localUser = formData.username
-        if (savePassword.value) {
-            config.localPass = formData.password
-        } else {
-            config.localPass = ""
-        }
-        localStorage.setItem("config", JSON.stringify(config))
-        isLoading.value = false
-        await router.push({name: "首页"})
-    } catch (e) {
-        isLoading.value = false
+  isLoading.value = true
+  try {
+    let msg = ""
+    if (!formData.username) {
+      msg = "请输入账号！"
+      accountRef.value.focus()
     }
+    if (!formData.password) {
+      pwdRef.value.focus()
+      msg = "请输入密码！"
+    }
+    if (msg) {
+      ElMessage({
+        message: msg,
+        type: 'warning',
+        grouping: true
+      })
+      isLoading.value = false
+      return
+    }
+    await login(formData)
+    await info()
+    config.savePassword = savePassword.value
+    config.localUser = formData.username
+    if (savePassword.value) {
+      config.localPass = formData.password
+    } else {
+      config.localPass = ""
+    }
+    localStorage.setItem("config", JSON.stringify(config))
+    isLoading.value = false
+    await router.push({name: "首页"})
+  } catch (e) {
+    isLoading.value = false
+  }
 }
 </script>
 
@@ -83,79 +104,79 @@ const onSubmit = async () => {
 <style scoped>
 
 .wrapper {
-    height: 32px;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    overflow: hidden;
+  height: 32px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
 }
 
 .submit-wrapper {
-    margin-top: 15px;
+  margin-top: 15px;
 
-    .submit-btn {
-        height: 32px;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #FFF;
-        font-weight: bold;
-        background-image: var(--background-color-50);
-        cursor: pointer;
-    }
+  .submit-btn {
+    height: 32px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+    font-weight: bold;
+    background-image: var(--background-color-50);
+    cursor: pointer;
+  }
 
-    .submit-btn:hover {
-        opacity: .8;
-    }
+  .submit-btn:hover {
+    opacity: .8;
+  }
 
-    .submit-btn:active {
-        opacity: .5;
-    }
+  .submit-btn:active {
+    opacity: .5;
+  }
 
-    #loading {
-        margin-right: 10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  #loading {
+    margin-right: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-    }
+  }
 }
 
 .save-p-wrapper {
-    margin-top: 5px;
-    margin-left: 5px;
+  margin-top: 5px;
+  margin-left: 5px;
 }
 
 .icon-click:active {
-    opacity: .5;
+  opacity: .5;
 }
 
 .input-wrapper {
-    background: rgba(209, 215, 229, 0.26);
-    margin-top: 15px;
+  background: rgba(209, 215, 229, 0.26);
+  margin-top: 15px;
 
-    .input-icon {
-        width: 30px;
-        height: 26px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  .input-icon {
+    width: 30px;
+    height: 26px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .input-con {
+    height: 26px;
+    flex: 1;
+
+    .input {
+      outline: none;
+      border: none;
+      line-height: 26px;
+      color: #9e9ea7;
+      background: transparent;
+      width: 100%;
     }
-
-    .input-con {
-        height: 26px;
-        flex: 1;
-
-        .input {
-            outline: none;
-            border: none;
-            line-height: 26px;
-            color: #9e9ea7;
-            background: transparent;
-            width: 100%;
-        }
-    }
+  }
 }
 
 </style>
--- minih-manager-web/src/components/login/form/PhoneCodeForm.vue
+++ minih-manager-web/src/components/login/form/PhoneCodeForm.vue
@@ -1,52 +1,52 @@
 <template>
+  <div class="wrapper input-wrapper">
+    <div class="input-icon">
+      <el-icon>
+        <Phone/>
+      </el-icon>
+    </div>
+    <div class="input-con">
+      <input ref="mobileRef" v-model="formData.mobile" class="input" placeholder="手机号" type="text">
+    </div>
+  </div>
+  <div class="wrapper code-wrapper">
     <div class="wrapper input-wrapper">
-        <div class="input-icon">
-            <el-icon>
-                <Phone/>
-            </el-icon>
-        </div>
-        <div class="input-con">
-            <input ref="mobileRef" v-model="formData.mobile" class="input" placeholder="手机号" type="text">
-        </div>
+      <div class="input-icon">
+        <el-icon>
+          <Lock/>
+        </el-icon>
+      </div>
+      <div class="input-con">
+        <input v-model="formData.code" class="input" placeholder="验证码" type="text"
+               @keydown.enter="onSubmit">
+      </div>
     </div>
-    <div class="wrapper code-wrapper">
-        <div class="wrapper input-wrapper">
-            <div class="input-icon">
-                <el-icon>
-                    <Lock/>
-                </el-icon>
-            </div>
-            <div class="input-con">
-                <input v-model="formData.code" class="input" placeholder="验证码" type="text"
-                       @keydown.enter="onSubmit">
-            </div>
-        </div>
-        <el-tooltip
-            :content="btnText!='获取验证码'?'验证码已发送，10分钟内输入有效！':'获取验证码'"
-            placement="top-start"
-            style="cursor: pointer"
-        >
-            <el-link :disabled="btnText!='获取验证码'" class="code-btn" style="width: 70px" type="primary"
-                     @click="getCode">
-                {{ btnText }}
-            </el-link>
-        </el-tooltip>
+    <el-tooltip
+        :content="btnText!='获取验证码'?'验证码已发送，10分钟内输入有效！':'获取验证码'"
+        placement="top-start"
+        style="cursor: pointer"
+    >
+      <el-link :disabled="btnText!='获取验证码'" class="code-btn" style="width: 70px" type="primary"
+               @click="getCode">
+        {{ btnText }}
+      </el-link>
+    </el-tooltip>
 
-    </div>
+  </div>
 
-    <div class="wrapper save-p-wrapper" style="margin-top: 5px;">
-    </div>
+  <div class="wrapper save-p-wrapper" style="margin-top: 5px;">
+  </div>
 
-    <div class="wrapper submit-wrapper">
-        <div class="submit-btn" @click="onSubmit">
-            <div v-show="isLoading" id="loading">
-                <el-icon :class="{'is-loading':isLoading}">
-                    <Loading/>
-                </el-icon>
-            </div>
-            登录
-        </div>
+  <div class="wrapper submit-wrapper">
+    <div class="submit-btn" @click="onSubmit">
+      <div v-show="isLoading" id="loading">
+        <el-icon :class="{'is-loading':isLoading}">
+          <Loading/>
+        </el-icon>
+      </div>
+      登录
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -60,7 +60,7 @@ import {ElMessage} from "element-plus";
 const config = JSON.parse(localStorage.getItem("config") || "{}")
 let btnText = ref("获取验证码")
 if (config.countdown && Number(config.countdown) > 0) {
-    btnText.value = config.countdown
+  btnText.value = config.countdown
 }
 let isLoading = ref(false)
 const mobileRef = ref();
@@ -69,69 +69,83 @@ const mobileRef = ref();
 const router = useRouter()
 
 const formData = reactive<Partial<FormInfo>>({
-    mobile: "",
-    code: "",
+  mobile: "",
+  code: "",
 })
 
 const countdownStart = (countdown: number = 60) => {
+  config.countdown = countdown
+  localStorage.setItem("config", JSON.stringify(config))
+  let i = setInterval(() => {
     config.countdown = countdown
     localStorage.setItem("config", JSON.stringify(config))
-    let i = setInterval(() => {
-        config.countdown = countdown
-        localStorage.setItem("config", JSON.stringify(config))
-        if (countdown <= 0) {
-            btnText.value = "获取验证码"
-            clearInterval(i)
-            return
-        }
-        countdown--
-        btnText.value = String(countdown)
-    }, 1000)
+    if (countdown <= 0) {
+      btnText.value = "获取验证码"
+      clearInterval(i)
+      return
+    }
+    countdown--
+    btnText.value = String(countdown)
+  }, 1000)
 }
 
 
 if (config.countdown && Number(config.countdown) > 0) {
-    countdownStart(Number(config.countdown))
+  countdownStart(Number(config.countdown))
+}
+const checkMobile = () => {
+  let msg: string = ""
+  if (!formData.mobile) {
+    mobileRef.value.focus()
+    return "请输入手机号！"
+  }
+  if (formData.mobile && !/^1\d{10}$/.test(formData.mobile)) {
+    return "手机号格式错误！"
+  }
 }
 
-
 const getCode = async () => {
-    let msg: string = ""
-
-    if (!formData.mobile) {
-        msg = "请输入手机号"
-        mobileRef.value.focus()
-    }
-    if (formData.mobile && !/^1\d{10}$/.test(formData.mobile)) {
-        msg = "手机号格式错误"
-    }
-    if (msg) {
-        ElMessage({
-            message: msg,
-            type: 'warning',
-            grouping:true
-        })
-        return;
-    }
-    countdownStart()
-    try {
-        await submitGetCode(formData)
-    } catch (e) {
-        isLoading.value = false
-    }
+  let msg: string = checkMobile()
+  if (msg) {
+    ElMessage({
+      message: msg,
+      type: 'warning',
+      grouping: true
+    })
+    return;
+  }
+  countdownStart()
+  try {
+    await submitGetCode(formData)
+  } catch (e) {
+    isLoading.value = false
+  }
 }
 
 
 const onSubmit = async () => {
-    isLoading.value = true
-    try {
-        await login(formData)
-        await info()
-        isLoading.value = false
-        await router.push({name: "首页"})
-    } catch (e) {
-        isLoading.value = false
+  isLoading.value = true
+  let msg: string = checkMobile()
+  try {
+    if (!formData.code) {
+      msg = "请输入验证码！"
+      mobileRef.value.focus()
     }
+    if (msg) {
+      ElMessage({
+        message: msg,
+        type: 'warning',
+        grouping: true
+      })
+      return;
+    }
+    await login(formData)
+    await info()
+    isLoading.value = false
+    await router.push({name: "首页"})
+  } catch (e) {
+    isLoading.value = false
+  }
 }
 </script>
 
@@ -139,82 +153,82 @@ const onSubmit = async () => {
 <style scoped>
 
 .wrapper {
-    height: 32px;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    overflow: hidden;
+  height: 32px;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
 }
 
 .submit-wrapper {
-    margin-top: 15px;
+  margin-top: 15px;
 
-    .submit-btn {
-        height: 32px;
-        width: 100%;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        color: #FFF;
-        font-weight: bold;
-        background-image: var(--background-color-50);
-        cursor: pointer;
-    }
+  .submit-btn {
+    height: 32px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #FFF;
+    font-weight: bold;
+    background-image: var(--background-color-50);
+    cursor: pointer;
+  }
 
-    .submit-btn:hover {
-        opacity: .8;
-    }
+  .submit-btn:hover {
+    opacity: .8;
+  }
 
-    .submit-btn:active {
-        opacity: .5;
-    }
+  .submit-btn:active {
+    opacity: .5;
+  }
 
-    #loading {
-        margin-right: 10px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
+  #loading {
+    margin-right: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-    }
+  }
 }
 
 .code-wrapper {
-    margin-top: 15px;
-    display: flex;
-    height: 32px;
-    align-items: center;
+  margin-top: 15px;
+  display: flex;
+  height: 32px;
+  align-items: center;
 
-    .code-btn {
-        height: 32px;
-        margin-left: 10px;
-    }
+  .code-btn {
+    height: 32px;
+    margin-left: 10px;
+  }
 }
 
 .input-wrapper {
-    background: rgba(209, 215, 229, 0.26);
+  background: rgba(209, 215, 229, 0.26);
 
 
-    .input-icon {
-        width: 30px;
-        height: 26px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  .input-icon {
+    width: 30px;
+    height: 26px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .input-con {
+    height: 26px;
+    flex: 1;
+
+    .input {
+      outline: none;
+      border: none;
+      line-height: 26px;
+      color: #9e9ea7;
+      background: transparent;
+      width: 100%;
     }
-
-    .input-con {
-        height: 26px;
-        flex: 1;
-
-        .input {
-            outline: none;
-            border: none;
-            line-height: 26px;
-            color: #9e9ea7;
-            background: transparent;
-            width: 100%;
-        }
-    }
+  }
 }
 
 </style>
